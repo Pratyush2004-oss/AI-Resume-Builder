083f9e514307fb42a137f6ec46b03bf8de6e93b5
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-10-13 16:46:26 +0530

    finalizing the webpage

--- src/dashboard/components/ResumeCard.jsx
+++ src/dashboard/components/ResumeCard.jsx
@@ -20,7 +20,7 @@ const ResumeCard = ({ resume, refreshData }) => {
         try {
             setLoading(true);
             const response = await db.delete(Resume).where(eq(Resume.resumeId, resume.resumeId));
-            if(response){
+            if (response) {
                 toast.success("Resume Deleted Successfully")
             }
         } catch (error) {
@@ -33,9 +33,17 @@ const ResumeCard = ({ resume, refreshData }) => {
         }
     }
     return resume && (
-        <div className='p-8 flex flex-col justify-between bg-gradient-to-b from-pink-100 via-purple-200 to-blue-200 rounded-lg h-[280px] hover:scale-105 border-t-4 border-t-red-400 transition-all hover:shadow-md'>
-            <h1 className='font-serif text-sm font-bold text-center'>{resume.title}</h1>
-            <img src='/public/cv.png' className='mx-auto size-28' />
+        <div
+            style={{
+                borderColor: resume.themeColor
+            }}
+            className='p-8 flex flex-col justify-between bg-gradient-to-b from-pink-100 via-purple-200 to-blue-200 rounded-lg h-[280px] hover:scale-105 border-t-[7px] transition-all hover:shadow-md'>
+            <h1 className='font-serif text-sm font-bold text-center'
+                style={{
+                    color: resume.themeColor
+                }}
+            >{resume.title}</h1>
+            <img src='/public/cv.png' className='mx-auto rounded-lg size-28' />
             <div className='flex justify-end gap-4'>
                 <Link to={`/dashboard/resume/${resume.resumeId}/edit`}><Edit className='p-0.5 text-white bg-purple-500 rounded-md' /></Link>
                 <Link to={`/my-resume/${resume.resumeId}/view`}><Download className='text-white bg-black rounded-md cursor-pointer p-0.5' /></Link>
